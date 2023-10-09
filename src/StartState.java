cd95151f2e45cc3e80ad26e1b68eb49f2058cca9
gunktp14 <exesport254618@gmail.com>
2023-10-09 21:47:25 +0700

    state pattern

---
+++ src/StartState.java
@@ -0,0 +1,12 @@
+
+public class StartState implements State{
+	public void doAction() {
+		System.out.println("Start State");
+	}
+
+	@Override
+	public void doAction2() {
+		System.out.println("HP 100");
+		
+	}
+}
